ac8e9121980cce64bba25c7a032404a319372dc8
jakaria <jakariaahmodmd@gmail.com>
2024-12-19 15:26:44 +0600

    poject complte

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log(Date());
+
+const jakaria = {
+    name:'JAKARIA',
+    age:20,
+    catagory:"one year academy",
+    main:{
+        Home:"atpara"
+    }
+}
+let {name,age,catagory,main:Home} = jakaria;
+console.log(jakaria);
+console.log(name);
+console.log(age);
+console.log(catagory);
+console.log(Home);
